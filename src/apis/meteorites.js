52560170cccbeab582e37b2506257f7fade521c7
Nicholas Tullbane <n.tullbane@tcu.edu>
2024-04-27 09:10:01 -0500

    Updates to Meteorite and SampleHistory APIs

--- src/apis/meteorites.js
+++ src/apis/meteorites.js
@@ -46,18 +46,4 @@ const fetchMeteorites = async () => {
 
 export { fetchMeteorites };
 
-const fetchSampleHistory = async (attribute, query) => {
-    const url = `http://localhost:8080/api/v1/samplehistory/find?${attribute}=${encodeURIComponent(query)}`;
-    try {
-        const response = await fetch(url);
-        if (!response.ok) {  // Check if the HTTP status code is 200-299
-            throw new Error(`HTTP error! Status: ${response.status}`);
-        }
-        return await response.json();
-    } catch (error) {
-        console.error("Error fetching sample history:", error);
-        throw error;  // Re-throw the error if you might want to handle it further up the call stack
-    }
-};
 
-export { fetchSampleHistory};
